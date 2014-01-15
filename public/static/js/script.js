839b452d5c0ea08bf5bd86f388aa2651b40ee23e
Aref Shafaei <aref.shafaei@gmail.com>
2014-01-15 09:24:43 +0330

    All of a sudden,I couldn not sleep and this is its result

--- public/static/js/script.js
+++ public/static/js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-
+	$.each($('.url-stats td:not(:has(i)):not(:has(div))'), function(i, d){
+		if($(d).html().length > 20 )
+			$(d).attr('title', $(d).html());
+			$(d).html($(d).html().substring(0,20)+ '...');
+	});
 });
 
 	
