20710a694c66b18082f7e3c1b6de3eea719296d2
Aref Shafaei <aref.shafaei@gmail.com>
2014-01-16 16:10:36 +0330

    UI:Fix bugs

--- public/static/js/script.js
+++ public/static/js/script.js
@@ -107,10 +107,11 @@ $(function(){
 	gazzle.connect();
 	gazzle.startIncreaseThread();
 
-	$.each($('.url-stats td:not(:has(i)):not(:has(div))'), function(i, d){
-		if($(d).html().length > 20 )
-			$(d).attr('title', $(d).html());
-			$(d).html($(d).html().substring(0,20)+ '...');
+	$.each($('.url-stats td a'), function(i, d){
+		if($(d).text().length > 45 ){
+			$(d).attr('title', $(d).text());
+			$(d).text($(d).text().substring(0,45)+ '...');
+		}
 	});
 
 	$("#crawl-start-btn").click(function(){
